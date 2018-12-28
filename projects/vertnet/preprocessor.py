def2d0f01741675a733ec08203bae5cc142dd243
John Deck <jdeck88@gmail.com>
2018-12-27 16:59:15 -0800

    updating configuration files for vertnet processing

--- projects/vertnet/preprocessor.py
+++ projects/vertnet/preprocessor.py
@@ -11,38 +11,23 @@ import uuid
 import pandas as pd
 from preprocessor import AbstractPreProcessor
 
-# haslength, haslifestage
-COLUMNS_MAP = {
-        'occurrenceid' : 'occurrence_id',
-        'genus' : 'genus',
-        'specificepithet' : 'specific_epithet',
-        'eventdate' : 'event_date',
-        'year' : 'year',
-        'decimallatitude' : 'latitude',
-        'decimallongitude' : 'longitude',
-        'haslength' : 'has_length',
-        'lengthtype' : 'length_type',
-        'lengthinmm' : 'length_in_mm',
-        'lengthunitsinferred' : 'length_units_inferred',
-        'hasmass' : 'has_mass',
-        'massing' : 'mass_in_g',
-        'massunitsinferred' : 'mass_units_inferred'
-}
-
 class PreProcessor(AbstractPreProcessor):
     def _process_data(self):
         num_processes = multiprocessing.cpu_count()
         chunk_size = 10000
         data = pd.read_csv(os.path.join(self.input_dir,'vertnet_input.csv'), sep=',', header=0,
-                usecols=['recordid','occurrenceid','genus','specificepithet','eventdate',
-                'year','decimallatitude','decimallongitude','lengthtype','lengthinmm'],
+                #usecols=['recordid','occurrenceid','genus','specificepithet','eventdate',
+                #'year','decimallatitude','decimallongitude','lengthtype','lengthinmm'],
                 engine='python')
 
         self._transform_data(data).to_csv(self.output_file, columns=self.headers, mode='a', header=False, index=False)
 
     def _transform_data(self, data):
-        data['record_id'] = data.apply(lambda x: uuid.uuid4(), axis=1)
-#        data.fillna("", inplace=True)  # replace all null values
-        return data.rename(columns=COLUMNS_MAP)
+        # Apply a unique UUID for every observation and element.  In Vertnet data, we don't track
+        # observations or elements so these are just UUIDs in both cases.
+        data['observationID'] = data.apply(lambda x: uuid.uuid4(), axis=1)
+        data['elementID'] = data.apply(lambda x: uuid.uuid4(), axis=1)
+
+        return data
 
 
